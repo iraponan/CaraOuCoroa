a7bfb9462d7bae76df649538138cfbe514ce0ac3
Iraponan Aires de Souza Marinho <iraponan.aires@outlook.com>
2022-01-20 21:42:15 -0300

    Finalizando...

--- app/src/main/java/br/eti/inovare/caraoucoroa/MainActivity.java
+++ app/src/main/java/br/eti/inovare/caraoucoroa/MainActivity.java
@@ -2,13 +2,35 @@ package br.eti.inovare.caraoucoroa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton botaoJogar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        botaoJogar = findViewById(R.id.imageButtonJogar);
+
+        botaoJogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ResultadoActivity.class);
+
+                int caraOuCoroa = new Random().nextInt(2);
+
+                intent.putExtra("caraoucoroa", caraOuCoroa);
+
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/br/eti/inovare/caraoucoroa/ResultadoActivity.java
+++ app/src/main/java/br/eti/inovare/caraoucoroa/ResultadoActivity.java
@@ -3,12 +3,38 @@ package br.eti.inovare.caraoucoroa;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class ResultadoActivity extends AppCompatActivity {
+
+    private ImageView imagemResultado;
+    private ImageButton buttonVoltar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado);
+
+        imagemResultado = findViewById(R.id.imageResultado);
+        buttonVoltar = findViewById(R.id.imageButtonVoltar);
+
+        Bundle bundle = getIntent().getExtras();
+
+        int caraOuCoroa = bundle.getInt("caraoucoroa");
+
+        if (caraOuCoroa == 0) {
+            imagemResultado.setImageResource(R.drawable.moeda_cara);
+        } else {
+            imagemResultado.setImageResource(R.drawable.moeda_coroa);
+        }
+
+        buttonVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
